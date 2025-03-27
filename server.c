36008b00c06c55f3069cd2930c5630cf270801bf
m7anj <45573549+mxnjv@users.noreply.github.com>
2025-03-27 22:30:26 +0000

    Added server.c and client.c files

---
+++ server.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/select.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <ctype.h>
+#include <sys/mman.h>
+#include <sys/uio.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main() {
+    return 0;
+}
